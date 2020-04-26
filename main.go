45d4ae2612756ac105b88a1671b9811141849a50
Ali <ak.msm91@gmail.com>
2020-04-26 09:35:10 -0400

    ch1

--- main.go
+++ main.go
@@ -15,12 +15,33 @@ func main() {
 	power = 9000
 	power1 := 8999
 	power3 := getPower()
+	name, power4 := "testname", 123987
+	value, exists := set_power("goku")
+	// in case which we dont need the first value we assign it to _
+	//_, exists := set_power("goku")
 
-	fmt.Println("its over %d \n", power, power1, power3)
+	if exists {
+		println("hey there", value)
+	}
+
+	fmt.Println("its over  \n", power, power1, power3, name, power4)
 
 }
 
 func getPower() int {
 	return 9020
+
+}
+
+func log(message string) {
+
+}
+
+//or add(a, b int) if they have the same type
+func add(a int, b int) int {
+	return 1
+}
+
+func set_power(name string) (int, bool) {
 
 }
